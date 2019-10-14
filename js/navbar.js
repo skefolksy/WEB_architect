c169c410cd942b844ccc40effb1cc1b730acee66
skefolksy <skefolksy@hotmail.com>
2019-10-14 17:49:42 +0700

    index mobile nav

---
+++ js/navbar.js
@@ -0,0 +1,5 @@
+function openMenu() {
+    document.getElementById("hamburger").classList.toggle("nav-is-active");
+    document.getElementById("menuMobile").classList.toggle("nav-show-menu");
+    document.getElementById("menuMobile").classList.toggle("nav-hide-menu");
+}
