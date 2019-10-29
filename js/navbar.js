0107e59ee23ccd3c611693076af5b13bf8c35095
skefolksy <skefolksy@hotmail.com>
2019-10-29 20:53:47 +0700

    update navbar

--- js/navbar.js
+++ js/navbar.js
@@ -2,4 +2,5 @@ function openMenu() {
     document.getElementById("hamburger").classList.toggle("nav-is-active");
     document.getElementById("menuMobile").classList.toggle("nav-show-menu");
     document.getElementById("menuMobile").classList.toggle("nav-hide-menu");
+    document.getElementById("navbar").classList.toggle("position-fixed");
 }
